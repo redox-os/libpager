b881883df08711ccbc5e7e68692a612711fbfcef
Jeremy Soller <jackpot51@gmail.com>
2016-07-31 18:47:05 -0600

    Remove welcome message Add redraw lambda, called at start and on keypress Change unwraps to trys Add line count Fix lines that are too long

--- src/lib.rs
+++ src/lib.rs
@@ -5,26 +5,45 @@ extern crate termion;
 use termion::{style, cursor, clear};
 use termion::event::Key;
 use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+use termion::raw::{IntoRawMode, RawTerminal};
 
+use std::cmp;
 use std::io::{self, Read, Write};
 
 /// Start the pager over some string.
 pub fn start<R, W>(stdin: R, stdout: W, title: &str, content: &str) -> io::Result<()>
     where R: Read,
           W: Write {
-    let mut stdout = stdout.into_raw_mode().unwrap();
+    let line_count = content.lines().count();
+    let term_size = termion::terminal_size()?;
 
-    write!(stdout, "{}{}{}{} Welcome to pager: j/k/q {}{}", clear::All, cursor::Goto(1, 1),
-           style::Invert, clear::CurrentLine, style::Reset, cursor::Hide)?;
+    let redraw = |stdout: &mut RawTerminal<W>, y: i32| -> io::Result<()> {
+        write!(stdout, "{}{}{} {} {}..{}/{} {}{}{}",
+                cursor::Goto(1, 1), style::Invert, clear::CurrentLine,
+                title, y + 1, y + term_size.1 as i32, line_count,
+                style::Reset, cursor::Goto(1, 2), clear::AfterCursor)?;
+        for line in content.lines().skip(y as usize).take(term_size.1 as usize - 1) {
+            if let Some(end) = line.char_indices().take(term_size.0 as usize).last() {
+                write!(stdout, "{}\r{}", &line[..end.0], cursor::Down(1))?;
+            } else {
+                write!(stdout, "\r{}", cursor::Down(1))?;
+            }
+        }
+        stdout.flush()?;
+        Ok(())
+    };
 
-    stdout.flush()?;
+    let mut stdout = stdout.into_raw_mode()?;
 
-    let mut y = 0i16;
+    write!(stdout, "{}", cursor::Hide)?;
+
+    let mut y = 0i32;
     let mut dy = 0;
 
+    redraw(&mut stdout, y)?;
+
     for i in stdin.keys() {
-        match i.unwrap() {
+        match i? {
             Key::Down | Key::Char('j') => dy = 1,
             Key::Up | Key::Char('k') => dy = -1,
             Key::Char('q') => break,
@@ -32,14 +51,10 @@ pub fn start<R, W>(stdin: R, stdout: W, title: &str, content: &str) -> io::Resul
         }
 
         if (dy > 0 || y != 0) && dy != 0 {
-            y += dy;
-            write!(stdout, "{}{}{} {} {}{}{}", cursor::Goto(1, 1), style::Invert, clear::CurrentLine,
-                   title, style::Reset, cursor::Goto(1, 2), clear::AfterCursor)?;
-            for line in content.lines().skip(y as usize).take(termion::terminal_size().unwrap().1 as usize) {
-                write!(stdout, "{}\r{}", line, cursor::Down(1))?;
-            }
-            stdout.flush()?;
+            y = cmp::max(0, cmp::min(y + dy, line_count as i32 - term_size.1 as i32));
             dy = 0;
+
+            redraw(&mut stdout, y)?;
         }
     }
 
