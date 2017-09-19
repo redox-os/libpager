5253bb4ba5be8ffc278b81b0a3f0558df51967db
Jeremy Soller <jackpot51@gmail.com>
2017-09-18 19:56:13 -0600

    Update termion

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,16 @@ use std::cmp;
 use std::io::{self, Read, Write};
 
 /// Start the pager over some string.
-pub fn start<R, W>(stdin: R, stdout: W, title: &str, content: &str) -> io::Result<()>
+pub fn start<R, W>(stdin: R, mut stdout: W, title: &str, content: &str) -> io::Result<()>
     where R: Read,
-          W: Write {
+          W: IntoRawMode {
     let line_count = content.lines().count();
     let term_size = termion::terminal_size()?;
 
     let redraw = |stdout: &mut RawTerminal<W>, y: i32| -> io::Result<()> {
         write!(stdout, "{}{}{} {} {}..{}/{} {}{}{}",
                 cursor::Goto(1, 1), style::Invert, clear::CurrentLine,
-                title, y + 1, y + (term_size.1 as i32 - 1), line_count,
+                title, y + 1, y + term_size.1 as i32 - 1, line_count,
                 style::Reset, cursor::Goto(1, 2), clear::AfterCursor)?;
         for line in content.lines().skip(y as usize).take(term_size.1 as usize - 1) {
             if let Some(end) = line.char_indices().nth(term_size.0 as usize) {
@@ -43,7 +43,7 @@ pub fn start<R, W>(stdin: R, stdout: W, title: &str, content: &str) -> io::Resul
     for i in stdin.keys() {
         match i? {
             Key::Down | Key::Char('j') => dy = 1,
-            Key::PageDown | Key::Char(' ') => dy = (term_size.1 as i32 - 1),
+            Key::PageDown | Key::Char(' ') => dy = term_size.1 as i32 - 1,
             Key::Char('d') => dy = (term_size.1 as i32 - 1)/2,
             Key::Up | Key::Char('k') => dy = -1,
             Key::PageUp | Key::Char('b') => dy = -(term_size.1 as i32 - 1),
